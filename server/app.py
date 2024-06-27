34a27851a3c541ab196f846e4b4fd5285973d9d2
Lyle Simmons <lylesimmons125@gmail.com>
2024-06-27 09:56:42 -0400

    lab complete

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session, jsonify, make_response
 from flask_restful import Resource
 
 from config import app, db, api
@@ -28,14 +28,41 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    
+    def get(self):
+        user_id = session.get('user_id')
+        if not user_id:
+            return '', 204
+        
+        user = User.query.get(user_id)
+        if user:
+            return {'username': user.username}, 200
+        return '', 204
 
 class Login(Resource):
-    pass
+    
+    def post(self):
+        data = request.get_json()
+        username = data['username']
+        password = data['password']
+        
+        user = User.query.filter_by(username=username).first()
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            return {'username': user.username}, 200
+        return {'error': 'Invalid credentials'}, 401
+
+
 
 class Logout(Resource):
-    pass
-
+    
+    def delete(self):
+        session.clear()
+        return '', 204
+    
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
 
